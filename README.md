8f04a8b272a44c9f2d4c84c58ddfddf211f35ffe
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2022-02-20 23:31:38 +0100

    Posting tests

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # cdn_db
 xelA's method of saving custom images inside a safe place, basically CDN, but with organization in mind
 
+## API
+```
+POST    /<folder>                   Upload file to storage
+DELETE  /<folder>/<filename>        Delete file from storage
+GET     /<folder>/<filename>        Get file from storage
+GET     /<folder>/<filename>/stats  Get file stats from storage
+```
+
 ## Requirements
 - Python 3.6 or above
---
+++ tests/upload_bytes.py
@@ -0,0 +1,30 @@
+import requests
+
+from random import randint
+from PIL import Image
+from io import BytesIO
+
+
+def create_dumb_image():
+    img = Image.new(
+        "RGB", (100, 100),
+        color=(randint(0, 255), randint(0, 255), randint(0, 255))
+    )
+    bio = BytesIO()
+    img.save(bio, "jpeg")
+    bio.seek(0)
+    return bio
+
+
+r = requests.delete(
+    "http://localhost:8080/test/test.jpg"
+)
+
+
+r = requests.post(
+    "http://localhost:8080/test",
+    headers={"user_id": "1337"},
+    files={"test.jpg": create_dumb_image()}
+)
+
+print("\n".join([str(r), str(r.content)]))
